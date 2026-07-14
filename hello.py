07d48a275f49c14100c4ed3aac0ded7a8cd034b0
prajwalpatil4 <prajwalpatil.research@gmail.com>
2026-07-14 15:46:09 +0530

    Add first Python hello program
    My journey to becoming a Python Backend Developer through daily coding practice, projects, and problem-solving.

---
+++ hello.py
@@ -0,0 +1,9 @@
+"""
+hello.py
+
+My first Python program.
+Prints a greeting and my learning interests.
+"""
+
+print("Hello, my name is Prajwal Patil.")
+print("I am currently learning Python and Investment in Financial Markets!\n")
