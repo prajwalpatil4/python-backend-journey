430379dffd1ebf9e6136b4021e18a8239e01966f
prajwalpatil4 <prajwalpatil.research@gmail.com>
2026-07-14 16:10:31 +0530

    Add variable examples and naming conventions
    Learned the basics of Python variables, including:
    - Creating and updating variables
    - Storing different data types (integer and string)
    - Understanding valid and invalid variable names
    - Added examples with comments for better understanding

---
+++ variables.py
@@ -0,0 +1,23 @@
+'''
+variable is like a box in the computer's memory 
+that you can store a single value.
+
+If we need to use variables later, the results must be stored in a variable.
+'''
+count = 40
+count = count + 2
+print(count) #which is 42
+
+# One more example
+
+fruit = 'Apple'
+print(fruit) #which is 'Apple'
+
+'''
+Valid and Invalid variable names
+
+Valid variable names:          Invalid variable names:
+my_variable                    my variable (space in between are not allowed)
+Counter                        2counter (cannot start with a number)
+myVariable                     my-variable (hyphen is not allowed)
+'''
