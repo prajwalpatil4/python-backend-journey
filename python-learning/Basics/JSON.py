f1e335ba4aeef764849f0920de89df42ddfbb9a5
Prajwal Patil <prajwalpatil.research@gmail.com>
2026-07-19 23:52:53 +0530

    Add JSON.py with examples for parsing and converting JSON data in Python

---
+++ python-learning/Basics/JSON.py
@@ -0,0 +1,32 @@
+"""
+JSON is a syntax for storing and exchanging data.
+
+JSON is text, written with JavaScript object notation.
+"""
+#Python has a built-in package called json, which can be used to work with JSON data.
+# import json
+
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+#Convert Python objects into JSON strings, and print the values:
+
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
